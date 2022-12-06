e0e8cec8695fbdde7fd77dea3f2752db4d7d5d6e
Macarena Ruiz <contactomruiz@yahoo.com>
2022-12-06 16:25:59 -0300

    player movemente + animation

--- Final Programación II/Assets/Scripts/PlayerMovement.cs
+++ Final Programación II/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
+    Animator animator;
+    bool isGrounded;
+    [SerializeField] private float jumpForce;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                Jump();
+            }
+        }
+
+        animator.SetBool("isGrounded", isGrounded);
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
