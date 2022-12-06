d0ba73465eeb61ee9fe1dcbce5d0c8156a48570b
Macarena Ruiz <contactomruiz@yahoo.com>
2022-12-06 20:23:57 -0300

    more spawn changes

--- Final Programación II/Assets/Scripts/GameManager.cs
+++ Final Programación II/Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject prefab1;
+    /*public GameObject prefab1;
     public GameObject prefab2;
     public GameObject prefab3;
 
@@ -44,5 +44,47 @@ public class GameManager : MonoBehaviour
         if (chooseObstacle == 2) { Instantiate(prefab2); }
         if (chooseObstacle == 3) { Instantiate(prefab3); }
 
+    }*/
+
+    [System.Serializable]
+    public struct SpawnableObject
+    {
+        public GameObject prefab;
+        [Range(0f, 1f)]
+        public float spawnChance;
+    }
+
+    public SpawnableObject[] objects;
+
+    public float minSpawnRate = 1f;
+    public float maxSpawnRate = 2f;
+
+    private void OnEnable()
+    {
+        Invoke(nameof(Spawn), Random.Range(minSpawnRate, maxSpawnRate));
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    private void Spawn()
+    {
+        float spawnChance = Random.value;
+
+        foreach (var obj in objects)
+        {
+            if (spawnChance < obj.spawnChance)
+            {
+                GameObject obstacle = Instantiate(obj.prefab);
+                obstacle.transform.position += transform.position;
+                break;
+            }
+
+            spawnChance -= obj.spawnChance;
+        }
+
+        Invoke(nameof(Spawn), Random.Range(minSpawnRate, maxSpawnRate));
     }
 }
