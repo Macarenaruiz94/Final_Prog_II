e7a12852a55c95e95526d949856040763cfdf1dc
Macarena Ruiz <contactomruiz@yahoo.com>
2022-12-07 20:18:30 -0300

    save

--- Final Programación II/Assets/Scripts/ObstaclesMovement.cs
+++ Final Programación II/Assets/Scripts/ObstaclesMovement.cs
@@ -6,23 +6,14 @@ public class ObstaclesMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     [SerializeField] private float speed;
-    [SerializeField] private float currentSpeed;
-    [SerializeField] private float maxSpeed;
-    [SerializeField] private float multiplierSpeed;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        currentSpeed = speed;
     }
 
     void Update()
     {
-        rb.velocity = new Vector2(-currentSpeed, 0);
-
-        if (currentSpeed < maxSpeed)
-        {
-            currentSpeed += multiplierSpeed;
-        }
+        rb.velocity = new Vector2(-speed, 0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
